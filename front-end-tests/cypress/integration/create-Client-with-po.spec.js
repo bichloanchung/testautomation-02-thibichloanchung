bb206cb09fa123802c2008b45acda90294ab9e53
Thi Bich Loan Chung <bichloan.1032170@gmail.com>
2021-04-22 18:40:53 +0200

    fix dashboardPage and create new test cases of create a new client

--- front-end-tests/cypress/integration/create-Client-with-po.spec.js
+++ front-end-tests/cypress/integration/create-Client-with-po.spec.js
@@ -3,13 +3,13 @@
 import * as loginFuncs from "../pages/loginPage"
 import * as logoutFuncs from "../pages/dashboardPage"
 
+
 describe('Testsuite', () =>{
-    
-    it('Perform a valid login and log out',() =>{
+    it('Perform new client creation', () =>{
         cy.visit('http://localhost:3000')
         loginFuncs.checkLoginPageTitle()
         loginFuncs.confirmLabel('Login')
         loginFuncs.performValidLogin('tester01', 'GteteqbQQgSr88SwNExUQv2ydb7xuf8c', 'Tester Hotel Overview')
-        logoutFuncs.performLogout('Login')
+       // logoutFuncs.performLogout('Login')
     })
 })
