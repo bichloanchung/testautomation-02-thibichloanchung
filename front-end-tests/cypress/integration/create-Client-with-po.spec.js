45f0f09a9fce46359d9c298bf49124c10506c869
Thi Bich Loan Chung <bichloan.1032170@gmail.com>
2021-04-25 22:22:29 +0200

    install faker and begin with create new client with random

--- front-end-tests/cypress/integration/create-Client-with-po.spec.js
+++ front-end-tests/cypress/integration/create-Client-with-po.spec.js
@@ -5,6 +5,12 @@ import * as dashboardFuncs from "../pages/dashboardPage"
 import * as clientsFuncs from "../pages/clientsPage"
 import * as createClientsFuncs from "../pages/clientNewPage"
 
+var faker = require('faker');
+
+let randomName = faker.name.findName();
+let randomMail = faker.internet.email();
+let randomPhone = faker.phone.phoneNumber();
+
 describe('Testsuite', () =>{
     beforeEach(() =>{
         cy.visit('http://localhost:3000')
@@ -16,9 +22,9 @@ describe('Testsuite', () =>{
     it('Perform new client creation', () =>{
         dashboardFuncs.viewClient('Clients')
         clientsFuncs.viewClientNew('New Client')
-        createClientsFuncs.createANewClient('Thi', 'thi@tester.se','0123456789', 'Clients')
-        //verify Last Client and log out
-        clientsFuncs.verifyLastClient('Thi', 'thi@tester.se', '0123456789')
+        createClientsFuncs.createANewClient(randomName, randomMail,randomPhone, 'Clients')
+        cy.log('Verify Last Client created with random faker  and log out --')
+        clientsFuncs.verifyLastClient(randomName, randomMail,randomPhone)
         dashboardFuncs.performLogout('Login')
     })
 })
