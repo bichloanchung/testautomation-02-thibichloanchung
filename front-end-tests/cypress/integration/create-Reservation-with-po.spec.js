908a553b8cae78b2d1fe80d34a925d7e916720cf
Thi Bich Loan Chung <bichloan.1032170@gmail.com>
2021-04-26 11:05:11 +0200

    create reservation with radom date from faker js

--- front-end-tests/cypress/integration/create-Reservation-with-po.spec.js
+++ front-end-tests/cypress/integration/create-Reservation-with-po.spec.js
@@ -5,7 +5,9 @@ import * as dashboardFuncs from '../pages/dashboardPage'
 import * as reserFuncs from '../pages/reservationsPage'
 import * as createResFuncs from '../pages/reserNewPage'
 
-describe('Testsuite', () =>{
+var faker = require('faker');
+
+describe('Testsuite', function(){
     beforeEach(() =>{
         cy.visit('http://localhost:3000')
         loginFuncs.checkLoginPageTitle()
@@ -13,10 +15,33 @@ describe('Testsuite', () =>{
         loginFuncs.performValidLogin('tester01', 'GteteqbQQgSr88SwNExUQv2ydb7xuf8c', 'Tester Hotel Overview')
     })
 
-    it('Perform a reservation creation', () =>{
+    it('Perform a reservation creation', function(){
         dashboardFuncs.viewReservation('Reservations')
         reserFuncs.createResAction('New Reservation')
-        createResFuncs.createReservation('2021-06-02', '2021-06-04', 'Thi (#3)', 'Floor 1, Room 101', 'ID: 2', 'Reservations')
+
+        //let randomDateCome = recentDate.getFullYear()+'-'+recentDate.getMonth()+'-'+recentDate.getDate()
+        //let randomDateLeave = futureDate.getFullYear()+'-'+futureDate.getMonth()+'-'+futureDate.getDate()
+        let dateRecent = faker.date.recent();
+            var year = dateRecent.getFullYear()
+            var month = dateRecent.getMonth()+1
+            var date = dateRecent.getDate()+1
+        
+            if (month < 10) month = '0'+ month
+            if (date < 10) date = '0' + date
+        
+            var randomDateCome = [year,month,date].join('-')
+        let dateFuture = faker.date.future();
+
+            var year = dateFuture.getFullYear()
+            var month = dateFuture.getMonth()+1
+            var date = dateFuture.getDate()+1
+            if (month < 10) month = '0'+ month
+            if (date < 10) date = '0' + date
+        
+            var randomDateLeave = [year,month,date].join('-')
+        
+        
+        createResFuncs.createReservation(randomDateCome, randomDateLeave, 'Jonas Hellman (#1)', 'Floor 1, Room 101', 'ID: 1', 'Reservations')
 
         dashboardFuncs.performLogout('Login')
 
